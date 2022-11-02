b947d10ad873a4320cf3bd552c4b48c395978d1f
leigp <907090585@qq.com>
2022-11-02 12:57:41 +0800

    feat: dropdown组件弹出框添加滚动条样式

--- src/eg/DropdownEg.vue
+++ src/eg/DropdownEg.vue
@@ -3,14 +3,23 @@
     <Dropdown>
       <span>click me</span>
       <template #pop>
-        <div>
+        <div class="item">
           <a href="javascript:;">item1</a>
         </div>
-        <div>
+        <div class="item">
           <a href="javascript:;">item2</a>
         </div>
-        <div>
+        <div class="item">
           <a href="javascript:;">item3</a>
+        </div>
+        <div class="item">
+          <a href="javascript:;">item4</a>
+        </div>
+        <div class="item">
+          <a href="javascript:;">item5</a>
+        </div>
+        <div class="item">
+          <a href="javascript:;">item6</a>
         </div>
       </template>
     </Dropdown>
@@ -29,6 +38,12 @@ import Dropdown from '@/lib/dropdown/Dropdown.vue'
   a {
     text-decoration: none;
     color: inherit;
+  }
+  .item {
+    padding: 4px 14px;
+    &:hover {
+      background-color: #f7f7f7;
+    }
   }
 }
 </style>
--- src/lib/dropdown/Dropdown.vue
+++ src/lib/dropdown/Dropdown.vue
@@ -40,7 +40,7 @@ const hide = () => {
     background-color: white;
     box-shadow: 0 0 30px 3px rgba(0, 0, 0, 0.1);
     min-width: 120px;
-    padding: 5px 8px;
+    padding: 8px 0;
     position: absolute;
     bottom: -2px;
     left: 0;
@@ -49,6 +49,24 @@ const hide = () => {
     opacity: 0;
     overflow: hidden;
     transition: all 250ms;
+    max-height: 8em;
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+      //整个滚动条的宽高设置
+      width: 4px; //宽高只有一个能生效，如果是横向滚动条高度生效，纵向滚动条宽度生效
+      height: 4px;
+    }
+    &::-webkit-scrollbar-thumb {
+      //滚动条滑块的设置
+      border-radius: 3px;
+      -moz-border-radius: 3px;
+      -webkit-border-radius: 3px;
+      background-color: #c3c3c3;
+    }
+    &::-webkit-scrollbar-track {
+      //滚动条轨道设置
+      background-color: transparent;
+    }
     &.opening {
       opacity: 1;
     }
