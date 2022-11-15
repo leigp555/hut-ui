d385d7ef964482bd1d1c8ec13351a77c0f40a68b
leigp <907090585@qq.com>
2022-11-15 22:53:03 +0800

    feat: 修改测试数据

--- src/eg/TreeEg.vue
+++ src/eg/TreeEg.vue
@@ -15,86 +15,91 @@ const onSelect = (position: string) => {
 const treeData = ref<TreeOptions[]>([
   {
     title: '0-0',
-    value: '0-0',
+    value: '0-0x',
     show: false,
     children: [
       {
         title: '0-0-0',
-        value: '0-0-0',
+        value: '0-0-0x',
         show: false,
         children: [
-          { title: '0-0-0-0', value: '0-0-0-0', color: 'red' },
-          { title: '0-0-0-1', value: '0-0-0-1', color: 'red' },
-          { title: '0-0-0-2', value: '0-0-0-2', color: 'red' }
+          { title: '0-0-0-0', value: '0-0-0-0x', color: 'red' },
+          { title: '0-0-0-1', value: '0-0-0-1x', color: 'red' },
+          { title: '0-0-0-2', value: '0-0-0-2x', color: 'red' }
         ]
       },
       {
         title: '0-0-1',
-        value: '0-0-1',
+        value: '0-0-1x',
         show: false,
         children: [
-          { title: '0-0-1-0', value: '0-0-1-0' },
-          { title: '0-0-1-1', value: '0-0-1-1' },
-          { title: '0-0-1-2', value: '0-0-1-2' }
+          { title: '0-0-1-0', value: '0-0-1-0x' },
+          { title: '0-0-1-1', value: '0-0-1-1x' },
+          { title: '0-0-1-2', value: '0-0-1-2x' }
         ]
       }
     ]
   },
   {
     title: '1-0',
-    value: '1-0',
+    value: '1-0x',
     show: false,
     children: [
       {
         title: '1-0-0',
-        value: '1-0-0',
+        value: '1-0-0x',
         show: false,
         children: [
           {
             title: '1-0-0-0',
-            value: '1-0-0-0',
+            value: '1-0-0-0x',
             href: 'javascript:;',
             color: '#1890ff'
           },
           {
             title: '1-0-0-1',
-            value: '1-0-0-1',
+            value: '1-0-0-1x',
             href: 'javascript:;',
             color: '#1890ff'
           },
-          { title: '1-0-0-2', value: '1-0-0-2', href: 'javascript:;', color: '#1890ff' }
+          {
+            title: '1-0-0-2',
+            value: '1-0-0-2x',
+            href: 'javascript:;',
+            color: '#1890ff'
+          }
         ]
       },
       {
         title: '1-0-1',
-        value: '1-0-1',
+        value: '1-0-1x',
         show: false,
         children: [
-          { title: '1-0-1-0', value: '1-0-1-0' },
-          { title: '1-0-1-1', value: '1-0-1-1' },
+          { title: '1-0-1-0', value: '1-0-1-0x' },
+          { title: '1-0-1-1', value: '1-0-1-1x' },
           {
             title: '1-0-1-2',
-            value: '1-0-1-2',
+            value: '1-0-1-2x',
             show: false,
             children: [
               {
                 title: '3-0-0',
-                value: '3-0-0',
+                value: '3-0-0x',
                 show: false,
                 children: [
-                  { title: '3-0-0-0', value: '3-0-0-0' },
-                  { title: '3-0-0-1', value: '3-0-0-1' },
-                  { title: '3-0-0-2', value: '3-0-0-2' }
+                  { title: '3-0-0-0', value: '3-0-0-0x' },
+                  { title: '3-0-0-1', value: '3-0-0-1x' },
+                  { title: '3-0-0-2', value: '3-0-0-2x' }
                 ]
               },
               {
                 title: '3-0-1',
-                value: '3-0-1',
+                value: '3-0-1x',
                 show: false,
                 children: [
-                  { title: '3-0-1-0', value: '3-0-1-0' },
-                  { title: '3-0-1-1', value: '3-0-1-1' },
-                  { title: '3-0-1-2', value: '3-0-1-2' }
+                  { title: '3-0-1-0', value: '3-0-1-0x' },
+                  { title: '3-0-1-1', value: '3-0-1-1x' },
+                  { title: '3-0-1-2', value: '3-0-1-2x' }
                 ]
               }
             ]
