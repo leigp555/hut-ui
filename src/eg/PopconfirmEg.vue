f0392e36a706bfdc334e1a8b4a9f25451b53229f
leigp <907090585@qq.com>
2022-11-19 07:50:44 +0800

    feat: 添加示例

--- src/eg/PopconfirmEg.vue
+++ src/eg/PopconfirmEg.vue
@@ -1,142 +1,187 @@
 <template>
   <div class="wrap">
-    <!--    <div class="ui-template-wrap">-->
-    <!--      <div class="top">-->
-    <!--        <Popconfirm placement="topLeft">-->
-    <!--          <template #content>-->
-    <!--            <p>Content</p>-->
-    <!--            <p>Content</p>-->
-    <!--          </template>-->
-    <!--          <template #title>-->
-    <!--            <span>Title</span>-->
-    <!--          </template>-->
-    <!--          <Button>TL</Button>-->
-    <!--        </Popconfirm>-->
-    <!--        <Popconfirm placement="topCenter">-->
-    <!--          <template #content>-->
-    <!--            <p>Content</p>-->
-    <!--            <p>Content</p>-->
-    <!--          </template>-->
-    <!--          <template #title>-->
-    <!--            <span>Title</span>-->
-    <!--          </template>-->
-    <!--          <Button>TL</Button>-->
-    <!--        </Popconfirm>-->
-    <!--        <Popconfirm placement="topRight">-->
-    <!--          <template #content>-->
-    <!--            <p>Content</p>-->
-    <!--            <p>Content</p>-->
-    <!--          </template>-->
-    <!--          <template #title>-->
-    <!--            <span>Title</span>-->
-    <!--          </template>-->
-    <!--          <Button>TL</Button>-->
-    <!--        </Popconfirm>-->
-    <!--      </div>-->
-    <!--      <div class="left-right">-->
-    <!--        <div class="left">-->
-    <!--          <Popconfirm placement="leftTop">-->
-    <!--            <template #content>-->
-    <!--              <p>Content</p>-->
-    <!--              <p>Content</p>-->
-    <!--            </template>-->
-    <!--            <template #title>-->
-    <!--              <span>Title</span>-->
-    <!--            </template>-->
-    <!--            <Button>TL</Button>-->
-    <!--          </Popconfirm>-->
-    <!--          <Popconfirm placement="leftCenter">-->
-    <!--            <template #content>-->
-    <!--              <p>Content</p>-->
-    <!--              <p>Content</p>-->
-    <!--            </template>-->
-    <!--            <template #title>-->
-    <!--              <span>Title</span>-->
-    <!--            </template>-->
-    <!--            <Button>TL</Button>-->
-    <!--          </Popconfirm>-->
-    <!--          <Popconfirm placement="leftBottom">-->
-    <!--            <template #content>-->
-    <!--              <p>Content</p>-->
-    <!--              <p>Content</p>-->
-    <!--            </template>-->
-    <!--            <template #title>-->
-    <!--              <span>Title</span>-->
-    <!--            </template>-->
-    <!--            <Button>TL</Button>-->
-    <!--          </Popconfirm>-->
-    <!--        </div>-->
-    <!--        <div class="right">-->
-    <!--          <Popconfirm placement="rightTop">-->
-    <!--            <template #content>-->
-    <!--              <p>Content</p>-->
-    <!--              <p>Content</p>-->
-    <!--            </template>-->
-    <!--            <template #title>-->
-    <!--              <span>Title</span>-->
-    <!--            </template>-->
-    <!--            <Button>TL</Button>-->
-    <!--          </Popconfirm>-->
-    <!--          <Popconfirm placement="rightCenter">-->
-    <!--            <template #content>-->
-    <!--              <p>Content</p>-->
-    <!--              <p>Content</p>-->
-    <!--            </template>-->
-    <!--            <template #title>-->
-    <!--              <span>Title</span>-->
-    <!--            </template>-->
-    <!--            <Button>TL</Button>-->
-    <!--          </Popconfirm>-->
-    <!--          <Popconfirm placement="rightBottom">-->
-    <!--            <template #content>-->
-    <!--              <p>Content</p>-->
-    <!--              <p>Content</p>-->
-    <!--            </template>-->
-    <!--            <template #title>-->
-    <!--              <span>Title</span>-->
-    <!--            </template>-->
-    <!--            <Button>TL</Button>-->
-    <!--          </Popconfirm>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--      <div class="bottom">-->
-    <!--        <Popconfirm placement="bottomLeft">-->
-    <!--          <template #content>-->
-    <!--            <p>Content</p>-->
-    <!--            <p>Content</p>-->
-    <!--          </template>-->
-    <!--          <template #title>-->
-    <!--            <span>Title</span>-->
-    <!--          </template>-->
-    <!--          <Button>TL</Button>-->
-    <!--        </Popconfirm>-->
-    <!--        <Popconfirm placement="bottomCenter">-->
-    <!--          <template #content>-->
-    <!--            <p>Content</p>-->
-    <!--            <p>Content</p>-->
-    <!--          </template>-->
-    <!--          <template #title>-->
-    <!--            <span>Title</span>-->
-    <!--          </template>-->
-    <!--          <Button>TL</Button>-->
-    <!--        </Popconfirm>-->
-    <!--        <Popconfirm placement="bottomRight">-->
-    <!--          <template #content>-->
-    <!--            <p>Content</p>-->
-    <!--            <p>Content</p>-->
-    <!--          </template>-->
-    <!--          <template #title>-->
-    <!--            <span>Title</span>-->
-    <!--          </template>-->
-    <!--          <Button>TL</Button>-->
-    <!--        </Popconfirm>-->
-    <!--      </div>-->
-    <!--    </div>-->
+    <div class="ui-template-wrap">
+      <div class="top">
+        <Popconfirm
+          placement="topLeft"
+          okText="Yes"
+          cancelText="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <template #title>
+            <p>{{ text }}</p>
+            <p>{{ text }}</p>
+          </template>
+          <Button>TL</Button>
+        </Popconfirm>
+        <Popconfirm
+          placement="topCenter"
+          okText="Yes"
+          cancelText="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <template #title>
+            <p>{{ text }}</p>
+            <p>{{ text }}</p>
+          </template>
+          <Button>TL</Button>
+        </Popconfirm>
+        <Popconfirm
+          placement="topRight"
+          okText="Yes"
+          cancelText="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <template #title>
+            <p>{{ text }}</p>
+            <p>{{ text }}</p>
+          </template>
+          <Button>TL</Button>
+        </Popconfirm>
+      </div>
+      <div class="left-right">
+        <div class="left">
+          <Popconfirm
+            placement="leftTop"
+            okText="Yes"
+            cancelText="No"
+            @confirm="confirm"
+            @cancel="cancel"
+          >
+            <template #title>
+              <p>{{ text }}</p>
+              <p>{{ text }}</p>
+            </template>
+            <Button>TL</Button>
+          </Popconfirm>
+          <Popconfirm
+            placement="leftCenter"
+            okText="Yes"
+            cancelText="No"
+            @confirm="confirm"
+            @cancel="cancel"
+          >
+            <template #title>
+              <p>{{ text }}</p>
+              <p>{{ text }}</p>
+            </template>
+            <Button>TL</Button>
+          </Popconfirm>
+          <Popconfirm
+            placement="leftBottom"
+            okText="Yes"
+            cancelText="No"
+            @confirm="confirm"
+            @cancel="cancel"
+          >
+            <template #title>
+              <p>{{ text }}</p>
+              <p>{{ text }}</p>
+            </template>
+            <Button>TL</Button>
+          </Popconfirm>
+        </div>
+        <div class="right">
+          <Popconfirm
+            placement="rightTop"
+            okText="Yes"
+            cancelText="No"
+            @confirm="confirm"
+            @cancel="cancel"
+          >
+            <template #title>
+              <p>{{ text }}</p>
+              <p>{{ text }}</p>
+            </template>
+            <Button>TL</Button>
+          </Popconfirm>
+          <Popconfirm
+            placement="rightCenter"
+            okText="Yes"
+            cancelText="No"
+            @confirm="confirm"
+            @cancel="cancel"
+          >
+            <template #title>
+              <p>{{ text }}</p>
+              <p>{{ text }}</p>
+            </template>
+            <Button>TL</Button>
+          </Popconfirm>
+          <Popconfirm
+            placement="rightBottom"
+            okText="Yes"
+            cancelText="No"
+            @confirm="confirm"
+            @cancel="cancel"
+          >
+            <template #title>
+              <p>{{ text }}</p>
+              <p>{{ text }}</p>
+            </template>
+            <Button>TL</Button>
+          </Popconfirm>
+        </div>
+      </div>
+      <div class="bottom">
+        <Popconfirm
+          placement="bottomLeft"
+          okText="Yes"
+          cancelText="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <template #title>
+            <p>{{ text }}</p>
+            <p>{{ text }}</p>
+          </template>
+          <Button>TL</Button>
+        </Popconfirm>
+        <Popconfirm
+          placement="bottomCenter"
+          okText="Yes"
+          cancelText="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <template #title>
+            <p>{{ text }}</p>
+            <p>{{ text }}</p>
+          </template>
+          <Button>TL</Button>
+        </Popconfirm>
+        <Popconfirm
+          placement="bottomRight"
+          okText="Yes"
+          cancelText="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <template #title>
+            <p>{{ text }}</p>
+            <p>{{ text }}</p>
+          </template>
+          <Button>TL</Button>
+        </Popconfirm>
+      </div>
+    </div>
 
-    <Popconfirm placement="topLeft" ok-text="Yes" cancel-text="No" @confirm="confirm">
+    <Popconfirm
+      placement="topLeft"
+      okText="Yes"
+      cancelText="No"
+      @confirm="confirm"
+      @cancel="cancel"
+    >
       <template #title>
         <p>{{ text }}</p>
         <p>{{ text }}</p>
+      </template>
+      <template #icon>
+        <SvgIcon name="error" width="1em" height="1em" fill="red" />
       </template>
       <Button>TL</Button>
     </Popconfirm>
@@ -146,39 +191,44 @@
 <script setup lang="ts">
 import Popconfirm from '@/lib/popconfirm/Popconfirm.vue'
 import Button from '@/lib/button/Button.vue'
+import SvgIcon from '@/lib/common/SvgIcon.vue'
 
 const text = 'Are you sure to delete this task?'
+
+const confirm = () => {
+  console.log('confirm')
+}
+const cancel = () => {
+  console.log('cancel')
+}
 </script>
 
 <style lang="scss">
 .wrap {
-  margin-top: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
 }
-//.wrap {
-//  display: flex;
-//  flex-direction: column;
-//  gap: 50px;
-//}
-//.ui-template-wrap {
-//  margin: 200px;
-//  width: 400px;
-//  .top,
-//  .bottom {
-//    display: flex;
-//    gap: 40px;
-//    justify-content: center;
-//    margin-bottom: 50px;
-//  }
-//  .left-right {
-//    display: flex;
-//    justify-content: space-between;
-//    margin-bottom: 50px;
-//    > .left,
-//    .right {
-//      display: flex;
-//      flex-direction: column;
-//      gap: 40px;
-//    }
-//  }
-//}
+.ui-template-wrap {
+  margin: 200px;
+  width: 400px;
+  .top,
+  .bottom {
+    display: flex;
+    gap: 40px;
+    justify-content: center;
+    margin-bottom: 50px;
+  }
+  .left-right {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 50px;
+    > .left,
+    .right {
+      display: flex;
+      flex-direction: column;
+      gap: 40px;
+    }
+  }
+}
 </style>
--- src/lib/popconfirm/Popconfirm.vue
+++ src/lib/popconfirm/Popconfirm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { withDefaults, defineProps, ref, toRefs } from 'vue'
+import { withDefaults, defineProps, ref } from 'vue'
 import Button from '../button/Button.vue'
 import SvgIcon from '@/lib/common/SvgIcon.vue'
 
@@ -17,41 +17,50 @@ type Placement =
   | 'rightCenter'
   | 'rightBottom'
 
-const props = withDefaults(
-  defineProps<{ placement: Placement; trigger?: 'hover' | 'click' }>(),
+const emits = defineEmits(['confirm', 'cancel'])
+withDefaults(
+  defineProps<{ placement: Placement; okText: string; cancelText: string }>(),
   {
     placement: 'topCenter',
-    trigger: 'hover'
+    okText: 'confirm',
+    cancelText: 'cancel'
   }
 )
 
-const { trigger } = toRefs(props)
-const shouldShow = ref<boolean>(true)
+const shouldShow = ref<boolean>(false)
 const onClick = () => {
-  if (trigger.value === 'click') shouldShow.value = true
+  shouldShow.value = true
+}
+const okClick = () => {
+  shouldShow.value = false
+  emits('confirm')
+}
+const cancelClick = () => {
+  shouldShow.value = false
+  emits('cancel')
 }
 </script>
 
 <template>
-  <div class="ui-popover-wrap" tabindex="-1" @click="onClick">
+  <div class="ui-popconfirm-wrap" tabindex="-1">
     <transition name="fade">
-      <div class="ui-popover-tip" :class="{ [placement]: true }" v-show="shouldShow">
-        <div class="ui-popover-content">
-          <div class="ui-popover-icon">
+      <div class="ui-popconfirm-tip" :class="{ [placement]: true }" v-show="shouldShow">
+        <div class="ui-popconfirm-content">
+          <div class="ui-popconfirm-icon">
             <slot name="icon" v-if="$slots.icon" />
             <SvgIcon name="warning" width="1em" height="1em" fill="#faad14" v-else />
           </div>
-          <p class="ui-popover-title">
+          <p class="ui-popconfirm-title">
             <slot name="title" />
           </p>
         </div>
-        <div class="ui-popover-actions">
-          <Button>yes</Button>
-          <Button type="default">no</Button>
+        <div class="ui-popconfirm-actions">
+          <Button @click="okClick">{{ okText }}</Button>
+          <Button type="default" @click="cancelClick">{{ cancelText }}</Button>
         </div>
       </div>
     </transition>
-    <div :style="{ 'pointer-events': 'none' }">
+    <div @click="onClick">
       <slot />
     </div>
   </div>
@@ -59,6 +68,4 @@ const onClick = () => {
 
 <style lang="scss">
 @import './index.scss';
-.ui-popover-wrap {
-}
 </style>
