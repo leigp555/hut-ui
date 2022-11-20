6cedbad191a158853ec3e93c7d86451f5a406e57
leigp <907090585@qq.com>
2022-11-20 11:20:56 +0800

    feat: 完成result组件

--- src/eg/ResultEg.vue
+++ src/eg/ResultEg.vue
@@ -12,15 +12,77 @@
         <Button key="buy">Buy Again</Button>
       </template>
     </Result>
+
+    <Result status="info">
+      <template #title> Successfully Purchased Cloud Server ECS! </template>
+      <template #extra>
+        <Button key="console" type="primary">Go Console</Button>
+      </template>
+    </Result>
+
+    <Result status="warning">
+      <template #title> Successfully Purchased Cloud Server ECS! </template>
+      <template #extra>
+        <Button key="console" type="primary">Go Console</Button>
+      </template>
+    </Result>
+    <Result status="error">
+      <template #title> Successfully Purchased Cloud Server ECS! </template>
+      <template #extra>
+        <Button key="console" type="primary">Go Console</Button>
+      </template>
+    </Result>
+
+    <Result>
+      <template #icon>
+        <SvgIcon name="notFound" width="250px" height="300px" />
+      </template>
+      <template #title> Successfully Purchased Cloud Server ECS! </template>
+      <template #extra>
+        <Button key="console" type="primary">Go Home</Button>
+      </template>
+    </Result>
+
+    <Result status="error">
+      <template #title> Submission Failed </template>
+      <template #subTitle>
+        Please check and modify the following information before resubmitting.</template
+      >
+      <template #extra>
+        <Button type="primary">Go Console</Button>
+        <Button>Buy Again</Button>
+      </template>
+      <template #desc>
+        <div class="desc">
+          <p style="font-size: 16px">
+            <strong>The content you submitted has the following error:</strong>
+          </p>
+          <p>
+            <SvgIcon name="cha" width="14px" height="14px" fill="red" />
+            Your account has been frozen
+            <a>Thaw immediately &gt;</a>
+          </p>
+          <p>
+            <SvgIcon name="cha" width="12px" height="12px" fill="red" />
+            Your account is not yet eligible to apply
+            <a>Apply Unlock &gt;</a>
+          </p>
+        </div>
+      </template>
+    </Result>
   </div>
 </template>
 
 <script setup lang="ts">
 import Result from '@/lib/result/Result.vue'
 import Button from '@/lib/button/Button.vue'
+import SvgIcon from '@/lib/common/SvgIcon.vue'
 </script>
 
 <style lang="scss">
 .ui-template-wrap {
+  .desc p {
+    margin-bottom: 1em;
+  }
 }
 </style>
--- src/lib/result/Result.vue
+++ src/lib/result/Result.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="ui-result-wrap">
-    <div class="ui-result-icon">
-      <SvgIcon name="success" width="1em" height="1em" />
+    <div class="ui-result-icon" v-if="status === 'success' && !$slots.icon">
+      <SvgIcon name="success" width="1em" height="1em" fill="#52c41a" />
     </div>
-    <div class="ui-result-title">
+    <div class="ui-result-icon" v-if="status === 'info' && !$slots.icon">
+      <SvgIcon name="info" width="1em" height="1em" fill="#1890ff" />
+    </div>
+    <div class="ui-result-icon" v-if="status === 'warning' && !$slots.icon">
+      <SvgIcon name="warning" width="1em" height="1em" fill="#faad14" />
+    </div>
+    <div class="ui-result-icon" v-if="status === 'error' && !$slots.icon">
+      <SvgIcon name="error" width="1em" height="1em" fill="#ff4d4f" />
+    </div>
+    <div class="ui-result-icon" v-if="$slots.icon">
+      <slot name="icon" />
+    </div>
+    <div class="ui-result-title" v-if="$slots.title">
       <slot name="title" />
     </div>
-    <div class="ui-result-subTitle">
+    <div class="ui-result-subTitle" v-if="$slots.subTitle">
       <slot name="subTitle" />
     </div>
-    <div class="ui-result-actions">
+    <div class="ui-result-actions" v-if="$slots.extra">
       <slot name="extra" />
+    </div>
+    <div class="ui-result-desc" v-if="$slots.extra">
+      <slot name="desc" />
     </div>
   </div>
 </template>
@@ -19,7 +34,7 @@
 import { withDefaults, defineProps } from 'vue'
 import SvgIcon from '@/lib/common/SvgIcon.vue'
 
-withDefaults(defineProps<{}>(), {})
+withDefaults(defineProps<{ status?: 'error' | 'info' | 'warning' | 'success' }>(), {})
 </script>
 
 <style lang="scss">
@@ -30,9 +45,6 @@ withDefaults(defineProps<{}>(), {})
     font-size: 72px;
     margin-bottom: 24px;
     text-align: center;
-    > svg {
-      fill: #52c41a;
-    }
   }
   > .ui-result-title {
     font-size: 24px;
@@ -51,6 +63,12 @@ withDefaults(defineProps<{}>(), {})
     display: flex;
     justify-content: center;
     gap: 8px;
+  }
+  > .ui-result-desc {
+    margin-top: 24px;
+    padding: 24px 40px;
+    background-color: #fafafa;
+    font-size: 14px;
   }
 }
 </style>
