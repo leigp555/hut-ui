b4ef37e38616bdfe3e57779c2f91cd1d03f61473
leigp <907090585@qq.com>
2022-11-23 23:48:35 +0800

    feat: 完成table组件的基本样式

---
+++ src/eg/TableEg.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="ui-template-wrap">
+    <Table :headSource="columns" :bodySource="data">
+      <template #tableHead="item">
+        <span style="color: red">{{ item.data }}</span>
+      </template>
+      <template #tableBody="item">
+        <span style="color: #1890ff">{{ item.data }}</span>
+      </template>
+    </Table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Table, { TableDataType } from '@/lib/table/Table.vue'
+
+const columns: TableDataType[] = [
+  {
+    label: 'Name',
+    key: 'name'
+  },
+  {
+    label: 'Age',
+    key: 'age'
+  },
+  {
+    label: 'Address',
+    key: 'address'
+  },
+  {
+    label: 'Tags',
+    key: 'tags'
+  }
+]
+
+const data: (
+  | TableDataType
+  | { name: string; age: number; address: string; tags: string[] }
+)[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer']
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser']
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher']
+  }
+]
+</script>
+
+<style lang="scss">
+.ui-template-wrap {
+  padding: 50px;
+}
+</style>
---
+++ src/lib/table/Table.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { withDefaults, defineProps, computed, toRefs } from 'vue'
+// import Button from '@/lib/button/Button.vue'
+
+export type TableDataType = {
+  key: string
+  label: string
+}
+
+const props = withDefaults(
+  defineProps<{ headSource?: TableDataType[]; bodySource?: TableDataType[] }>(),
+  {
+    headSource: () => [],
+    bodySource: () => []
+  }
+)
+const { headSource } = toRefs(props)
+const headArr = computed<string[]>(() => {
+  return headSource.value.map((item) => {
+    return item.key
+  })
+})
+</script>
+
+<template>
+  <table class="ui-table-wrap">
+    <thead class="ui-table-thead">
+      <tr>
+        <th v-for="item in headSource" :key="item.key">
+          <slot name="tableHead" :data="item.label" />
+        </th>
+      </tr>
+    </thead>
+    <tbody class="ui-table-tbody">
+      <tr v-for="item in bodySource" :key="item.key">
+        <td v-for="data in headArr" :key="data">
+          <slot name="tableBody" :data="item[data]" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <slot />
+</template>
+<style lang="scss">
+$font_color: #000000d9;
+$background_color: #fafafa;
+$border_color: #f0f0f0;
+.ui-table-wrap {
+  table-layout: auto;
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+  color: $font_color;
+  line-height: 1.5715;
+  font-size: 14px;
+  background: #fff;
+  border-radius: 2px;
+  > .ui-table-thead {
+    th {
+      font-weight: 500;
+      background: $background_color;
+      border-bottom: 1px solid $border_color;
+      transition: background 0.3s;
+      padding: 16px;
+      overflow-wrap: break-word;
+    }
+  }
+  > .ui-table-tbody {
+    vertical-align: middle;
+    > tr {
+      &:hover {
+        background: $background_color;
+        transition: background 0.3s;
+      }
+      td {
+        border-bottom: 1px solid $border_color;
+        padding: 16px;
+        overflow-wrap: break-word;
+      }
+    }
+  }
+}
+</style>
