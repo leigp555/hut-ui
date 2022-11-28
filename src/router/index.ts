fb2092a2b6b11c5c249adee82ab8069205d84a61
leigp <907090585@qq.com>
2022-11-29 02:29:55 +0800

    fix: 修复button引入错误

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import Home from '@/views/Home.vue'
 const Guide = () => import('@/views/Guide.vue')
 const Components = () => import('@/views/Components.vue')
 const NotFound = () => import('@/views/NotFound.vue')
-const Button = () => import('@/eg/button/index.vue')
+const Button = () => import('@/eg/button/Index.vue')
 
 const routes: Array<RouteRecordRaw> = [
   {
