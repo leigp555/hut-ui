813dd90d70846c6d8870eee34c0088533ddeeb71
leigp <907090585@qq.com>
2022-12-02 20:44:13 +0800

    docs: radio组件示例创建

---
+++ src/eg/radio/Intro.vue
@@ -0,0 +1,28 @@
+<template>
+  <Typography>
+    <Title :level="4">简介</Title>
+    <Paragraph>
+      <p>对文章内容进行排版，统一文章样式</p>
+    </Paragraph>
+    <Title :level="4">组件概述</Title>
+    <Paragraph>
+      <ul>
+        <li>Typography：组件包裹</li>
+        <li>Paragraph：段落</li>
+        <li>Text：用于加粗，标记</li>
+        <li>Title：标题可设置h1-h6</li>
+      </ul>
+    </Paragraph>
+    <Title :level="4">使用场景</Title>
+    <Paragraph>
+      <ul>
+        <li>当需要展示标题、段落、列表内容时使用，如文章/博客/日志的文本样式。</li>
+        <li>当需要一列基于文本的基础操作时，如拷贝/省略/可编辑。</li>
+      </ul>
+    </Paragraph>
+  </Typography>
+</template>
+
+<script lang="ts" setup>
+import { Typography, Paragraph, Title } from '@/lib/index'
+</script>
