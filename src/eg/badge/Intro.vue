e51d4b9de998f9536abcd0e13c6cc49d66aed427
leigp <907090585@qq.com>
2022-12-05 02:25:24 +0800

    feat: 完成badge组件示例

---
+++ src/eg/badge/Intro.vue
@@ -0,0 +1,26 @@
+<template>
+  <Typography>
+    <Title>简介</Title>
+    <Paragraph>
+      <p>图标右上角的圆形徽标数字。</p>
+    </Paragraph>
+    <Title>组件概览</Title>
+    <Paragraph>
+      <ul>
+        <li>Badge: 主要组件</li>
+      </ul>
+    </Paragraph>
+    <Title>使用场景</Title>
+    <Paragraph>
+      <ul>
+        <li>
+          一般出现在通知图标或头像的右上角，用于显示需要处理的消息条数，通过醒目视觉形式吸引用户处理。
+        </li>
+      </ul>
+    </Paragraph>
+  </Typography>
+</template>
+
+<script lang="ts" setup>
+import { Typography, Paragraph, Title } from '@/lib/index'
+</script>
---
+++ src/eg/badge/demo/DemoOne.vue
@@ -0,0 +1,52 @@
+<demo>
+基本用法
+</demo>
+
+<template>
+  <div class="wrap">
+    <Badge>
+      <Avatar shape="square">
+        <template #icon>
+          <SvgIcon name="user" width="2em" height="2em" />
+        </template>
+      </Avatar>
+      <template #count
+        ><span class="count">{{ count > 99 ? '99+' : count }}</span></template
+      >
+    </Badge>
+    <Badge>
+      <Avatar shape="square">
+        <template #icon>
+          <SvgIcon name="user" width="2em" height="2em" />
+        </template>
+      </Avatar>
+      <template #count
+        ><span class="count">{{ 0 }}</span></template
+      >
+    </Badge>
+    <Badge>
+      <Avatar shape="square">
+        <template #icon>
+          <SvgIcon name="user" width="2em" height="2em" />
+        </template>
+      </Avatar>
+      <template #count
+        ><span class="count">{{ 55 }}</span></template
+      >
+    </Badge>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Badge, SvgIcon, Avatar } from '@/lib'
+
+const count = ref<number>(100)
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  gap: 20px;
+}
+</style>
