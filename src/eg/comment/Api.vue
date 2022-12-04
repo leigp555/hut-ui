6daa1290f8e820a83bebfc3f910b53885d348c85
leigp <907090585@qq.com>
2022-12-05 03:57:33 +0800

    feat: 完成comment组件示例

---
+++ src/eg/comment/Api.vue
@@ -0,0 +1,21 @@
+<template>
+  <ApiIntro :attr-content-arr="attrContentArr" :event-content="eventContent" />
+</template>
+
+<script lang="ts" setup>
+import ApiIntro from '@/components/ApiIntro.vue'
+
+const attrContent1 = [
+  {
+    rowIndex: 1,
+    属性: 'data',
+    说明: '数据',
+    类型: 'CommentData[]',
+    默认值: '[]'
+  }
+]
+
+const eventContent = []
+
+const attrContentArr = [{ componentName: 'Comment', attrContent: attrContent1 }]
+</script>
---
+++ src/eg/comment/Intro.vue
@@ -0,0 +1,24 @@
+<template>
+  <Typography>
+    <Title>简介</Title>
+    <Paragraph>
+      <p>对网站内容的反馈、评价和讨论。</p>
+    </Paragraph>
+    <Title>组件概览</Title>
+    <Paragraph>
+      <ul>
+        <li>Comment: 主要组件</li>
+      </ul>
+    </Paragraph>
+    <Title>使用场景</Title>
+    <Paragraph>
+      <ul>
+        <li>评论组件可用于对事物的讨论，例如页面、博客文章、问题等等。</li>
+      </ul>
+    </Paragraph>
+  </Typography>
+</template>
+
+<script lang="ts" setup>
+import { Typography, Paragraph, Title } from '@/lib/index'
+</script>
