95755e7d5f7b5d2970e4d428d7aa39a126f71baa
leigp <907090585@qq.com>
2022-12-05 21:04:27 +0800

    feat: 完成backTop组件示例

---
+++ src/eg/backtop/Intro.vue
@@ -0,0 +1,25 @@
+<template>
+  <Typography>
+    <Title>简介</Title>
+    <Paragraph>
+      <p>返回页面顶部的操作按钮。</p>
+    </Paragraph>
+    <Title>组件概览</Title>
+    <Paragraph>
+      <ul>
+        <li>BackTop: 主要组件</li>
+      </ul>
+    </Paragraph>
+    <Title>使用场景</Title>
+    <Paragraph>
+      <ul>
+        <li>当页面内容区域比较长时；</li>
+        <li>当用户需要频繁返回顶部查看相关内容时。</li>
+      </ul>
+    </Paragraph>
+  </Typography>
+</template>
+
+<script lang="ts" setup>
+import { Typography, Paragraph, Title } from '@/lib/index'
+</script>
