efe83fcb9b414b9b7e93bebf3fb9479f3f7bfdc1
leigp <907090585@qq.com>
2022-12-30 15:54:17 +0800

    feat: 完善404页面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,1 +1,34 @@
-<template>notFound</template>
+<template>
+  <Result style="margin-top: 20px">
+    <template #icon>
+      <SvgIcon name="notFound" width="250px" height="250px" />
+    </template>
+    <template #title>
+      <p class="substr">File not found</p>
+      <p class="substr">
+        The site configured at this address does not contain the requested file.
+      </p>
+    </template>
+    <template #extra>
+      <Button key="console" type="primary" @click="onClick">Go back</Button>
+    </template>
+  </Result>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { Result, Button, SvgIcon } from '@/lib'
+
+const router = useRouter()
+
+const onClick = () => {
+  router.back()
+}
+</script>
+
+<style lang="scss" scoped>
+.substr {
+  color: rgba(0, 0, 0, 0.5);
+  font-size: 16px;
+}
+</style>
